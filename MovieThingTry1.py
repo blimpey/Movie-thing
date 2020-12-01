95189b46b3044d1ae4e4ad562cf4ab9d93b8b10a
Michael Wenk <mikewenk@gmail.com>
2020-11-30 23:57:58 -0800

    renamed and updated path

--- MovieThingTry1.py
+++ MovieThingTry1.py
@@ -5,7 +5,7 @@ import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
-df = pd.read_csv("movie_dataset.csv")
+df = pd.read_csv("data/movie_dataset.csv")
 #df = pd.read_csv("C:\Users\emwo1\Downloads\SampleData1\movie_dataset.csv")
 
 features = ['keywords', 'cast', 'genres', 'director']

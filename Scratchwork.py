e25438fbb03c438a238c9d8b600fc307cddeec53
blimpey <squadilla@gmail.com>
2020-12-01 09:27:33 -0800

    Update Scratchwork.py

--- Scratchwork.py
+++ Scratchwork.py
@@ -1,18 +1,51 @@
+#libraries used
 import numpy as np
 import pandas as pd
 
-ratings_data = pd.read_csv("C:\Users\emwo1\Downloads\Movies\ratings.csv")
+#convert things to vectors with these to run cosine similarity
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+#import API decider, or API that can decide where to stream movies
+
+#grab data from files
+movies = pd.read_csv("movies.csv")
+ratings_data = pd.read_csv("C:\Users\emwo1\Downloads\SampleData1\ratings.csv")
+ratings_data.head()
 
 movie_names = pd.read_csv("E:\Datasets\ml-latest-small\\movies.csv")
+movie_names.head()
 
+categories = ['keywords',  'genres', ]
+for feature in categories:
+    df[feature] = df[feature].fillna('')
 
-// recall from list of hosts, are they necessary? 
-hosts = [Kinetic, John, Vanessa, Dan]
+#data pull
+
+#combining features (multiple categories into 1 field) from the tutorial on cosine//
+def combined_categories(row):
+    return row['keywords']+" "+row['cast']+" "+row['genres']+" "+row['director']
+df["combined_categories"] = df.apply(combined_categories, axis =1)
+
+#sorting by titles
+movie_data = pd.merge(ratings_data, movie_names, on='movieId')
+
+movie_data.head()
+
+movie_data.groupby('title')['rating'].mean().head()
+movie_data.groupby('genre')['rating'].mean().head()
+movie_data.groupby('title')['rating'].mean().sort_values(ascending=False).head()
+
+movie_data.groupby('title')['rating'].count().sort_values(ascending=False).head()
+
+# recall from list of hosts, are they necessary? 
+hostname = [Kinetic, John, Vanessa, Dan]
 Kinetic = [ "action" , " cult" , "monster" , "animation", "b-movie"]
 John = ["documentary" , "food", "action" , "cult" , "animation"]
 Dan = ["horror", "documentary",  "animation",  "b-movie",  "martial-arts"]
 Vanessa = ["horror", "action", "cult", "drama", "animation", "b-movie"] 
-
+-----
+#data validation for hosts
 hostlist = {
 "Vanessa":  ["horror", "action", "cult", "drama", "animation", "b-movie"],
 "Kinetic":  [ "action" , " cult" , "monster" , "animation", "b-movie"],
@@ -24,25 +57,20 @@ hostlist = {
 print ("Who is hosting?")
 host = input()
 
+
 if (hostlist.get(host) is None ):
     print("There is no valid host")
 else :
     print("Welcome back (" + host + ")!")
-    
-movie_names.head()
+	other module.get 
+print("You should watch " (Title1) + " or " + (Title2)"
+	-------
 
-movie_data = pd.merge(ratings_data, movie_names, on='movieId')
+print ("Who is hosting?") 
+input(host)
+// asking for the value of host , if no, returns bad data 
+if (host.get(host) == null): 
+    print("There is no valid host")
 
-movie_data.head()
-
-movie_data.groupby('title')['rating'].mean().head()
-
-movie_data.groupby('title')['rating'].mean().sort_values(ascending=False).head()
-
-movie_data.groupby('title')['rating'].count().sort_values(ascending=False).head()
-ratings_data.head()
-input("Who is hosting?)
-?
-
-#give two options
-Print("You can watch" /n  )
+Host = 
+Print  ("You can watch" /n  )
